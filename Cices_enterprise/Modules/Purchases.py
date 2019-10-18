6d64321928c47117f6bd39b88046bd68f187e6bc
tysonic <taibonicholas@gmail.com>
2019-10-18 23:34:22 +0300

    created a drop down list for items

--- Cices_enterprise/Modules/Purchases.py
+++ Cices_enterprise/Modules/Purchases.py
@@ -5,21 +5,21 @@ class Purchases(db.Model):
     __tablename__ = "Purchases"
     Id = db.Column(db.Integer, primary_key =True, index=True)
     price = db.Column(db.String)
-    date_purchased = db.Column(db.DateTime)
-    quantity_purchased = db.Column(db.Integer)
-    purchased_by = db.Column(db.String)
-    purchased_on = db.Column(db.DateTime)
-    recorded_at = db.Column(db.DateTime)
+    # date_purchased = db.Column(db.DateTime)
+    # quantity_purchased = db.Column(db.Integer)
+    # purchased_by = db.Column(db.String)
+    # purchased_on = db.Column(db.DateTime)
+    # recorded_at = db.Column(db.DateTime)
     item_purchased = db.Column(db.String, db.ForeignKey('Items.Id'))
 
     def __init__(self, price, date_purchased, quantity_purchased, purchased_by, purchased_on, recorded_at, item_purchased):
         self.price = price
-        self.date_purchased= date_purchased
-        self.quantity_purchased = quantity_purchased
-        self.purchased_by = purchased_by
-        self.purchased_on = purchased_on
-        self.recorded_at = recorded_at
+        # self.date_purchased= date_purchased
+        # self.quantity_purchased = quantity_purchased
+        # self.purchased_by = purchased_by
+        # self.purchased_on = purchased_on
+        # self.recorded_at = recorded_at
         self.item_purchased = item_purchased
 
     def __repr__(self):
-        f"{self.price,self.date_purchased, self.quantity_purchased,  self.purchased_by, self.purchased_on, self.recorded_at, self.item_purchased}"
+        f"{self.price, self.item_purchased}"
--- Cices_enterprise/Purchases/Views.py
+++ Cices_enterprise/Purchases/Views.py
@@ -4,7 +4,7 @@ from Cices_enterprise import db
 from Cices_enterprise.Modules.Items import Items
 from Cices_enterprise.Modules.Purchases import Purchases
 from flask import render_template, redirect, flash, url_for
-from Cices_enterprise.Computations.Constants import item_form_choice
+from Cices_enterprise.Computations.dropdowns import item_form_choice
 purchase_blueprint = Blueprint("Purchases",__name__, template_folder="templates/purchases")
 
 
@@ -17,15 +17,10 @@ def list_of_purchases():
 @purchase_blueprint.route("/purchases/add purchase record", methods = ["GET","POST"])
 def add_purchase_record():
     form = AddPurchase()
-    # item_list = []
-    # select_list =[]
-    # items = Items.query.all()
-    # for item in items:
-    #     item_list.append((item.Id,item.Name))
     item_form_choice(form)
     if form.validate_on_submit():
-        new_purchase = Purchases(item_purchased=form.item.choices,price=form.price.data, date_purchased=None, quantity_purchased=form.quantity.data,
-                                 purchased_by='', purchased_on=None, recorded_at=None )
+        new_purchase = Purchases(item_purchased=form.item.data,price=form.price.data,
+                                  )
         db.session.add(new_purchase)
         db.session.commit()
         return redirect(url_for("list_of_purchases"))
